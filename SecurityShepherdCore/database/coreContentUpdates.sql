0fb5d3a28aa6a60f70f8e60f0f6bbfbb98f2cc8b
Christopher McMahon <cmcmahon@walmartlabs.com>
2017-02-02 15:06:18 -0800

    Adding re-runnable content update SQL
    - Assign all modules with rank < 45 to week 1
    - Assign all sql injection modules to week 2
    - Set all modules unbound to a week to closed

---
+++ SecurityShepherdCore/database/coreContentUpdates.sql
@@ -0,0 +1,11 @@
+USE `core` ;
+
+-- This file contains updates to existing instance data
+
+UPDATE modules SET week = 1 WHERE incrementalRank < 45;
+UPDATE modules SET week = 2 WHERE moduleNameLangPointer LIKE 'sql.injection%';
+
+UPDATE modules SET moduleStatus = 'closed' WHERE week is null;
+UPDATE modules SET moduleStatus = 'open' WHERE week is not null;
+
+COMMIT;
