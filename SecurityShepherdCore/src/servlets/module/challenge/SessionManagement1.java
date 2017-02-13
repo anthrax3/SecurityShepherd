fdb733c02e8160d4560c939f7a4bdbad01d870a9
Christopher McMahon <cmcmahon@walmartlabs.com>
2017-02-12 20:28:26 -0800

    Hard-coded hash value omits trailing ==

--- SecurityShepherdCore/src/servlets/module/challenge/SessionManagement1.java
+++ SecurityShepherdCore/src/servlets/module/challenge/SessionManagement1.java
@@ -22,19 +22,19 @@ import utils.Validate;
  * Session Management Challenge One
  * <br/><br/>
  * This file is part of the Security Shepherd Project.
- * 
+ *
  * The Security Shepherd project is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.<br/>
- * 
+ *
  * The Security Shepherd project is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.<br/>
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with the Security Shepherd project.  If not, see <http://www.gnu.org/licenses/>. 
+ * along with the Security Shepherd project.  If not, see <http://www.gnu.org/licenses/>.
  * @author Mark Denihan
  *
  */
@@ -47,22 +47,22 @@ public class SessionManagement1 extends HttpServlet
 	private static String levelResult = "db7b1da5d7a43c7100a6f01bb0c";
 	/**
 	 * Users must take advance of the broken session management in this application by modifying the tracking cookie "checksum" which is encoded in base 64. They must modify this cookie to be equal to administrator to access the result key.
-	 * @param upgraeUserToAdmin Red herring 
-	 * @param returnPassword Red herring 
-	 * @param adminDetected Red herring 
+	 * @param upgraeUserToAdmin Red herring
+	 * @param returnPassword Red herring
+	 * @param adminDetected Red herring
 	 * @param checksum Cookie encoded base 64 that manages who is signed in to the sub schema
 	 */
-	public void doPost (HttpServletRequest request, HttpServletResponse response) 
+	public void doPost (HttpServletRequest request, HttpServletResponse response)
 	throws ServletException, IOException
 	{
-		PrintWriter out = response.getWriter();  
+		PrintWriter out = response.getWriter();
 		out.print(getServletInfo());
-		
+
 		//Translation Stuff
 		Locale locale = new Locale(Validate.validateLanguage(request.getSession()));
 		ResourceBundle errors = ResourceBundle.getBundle("i18n.servlets.errors", locale);
 		ResourceBundle bundle = ResourceBundle.getBundle("i18n.servlets.challenges.sessionManagement.sessionManagement1", locale);
-		
+
 		try
 		{
 			//Setting IpAddress To Log and taking header for original IP if forwarded from proxy
@@ -87,15 +87,15 @@ public class SessionManagement1 extends HttpServlet
 				if(theCookie != null)
 				{
 					log.debug("Cookie value: " + theCookie.getValue());
-					
-					if(theCookie.getValue().equals("dXNlclJvbGU9YWRtaW5pc3RyYXRvcg"))
+
+					if(theCookie.getValue().startsWith("dXNlclJvbGU9YWRtaW5pc3RyYXRvcg"))
 					{
 						log.debug("Challenge Complete");
 						// Get key and add it to the output
 						String userKey = Hash.generateUserSolution(levelResult, (String)ses.getAttribute("userName"));
 						htmlOutput = "<h2 class='title'>" + bundle.getString("response.adminClub") + "</h2>" +
 								"<p>" +
-								bundle.getString("response.welcomeAdmin") + 
+								bundle.getString("response.welcomeAdmin") +
 								"<a>" + userKey + "</a>" +
 								"</p>";
 					}
@@ -109,8 +109,8 @@ public class SessionManagement1 extends HttpServlet
 						hackDetected = !(request.getParameter("adminDetected").toString().equalsIgnoreCase("false") &&
 										request.getParameter("adminDetected").toString().equalsIgnoreCase("false") &&
 										request.getParameter("adminDetected").toString().equalsIgnoreCase("false"));
-					
-					
+
+
 					if(!hackDetected)
 					{
 						htmlOutput = "<h2 class='title'>" + bundle.getString("response.notAdmin") + "</h2>" +
