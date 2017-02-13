c00878af0b060130cda51c8a170de0e72a9ed12b
Christopher McMahon <cmcmahon@walmartlabs.com>
2017-02-12 20:32:01 -0800

    Open week 4 challenges

--- SecurityShepherdCore/database/coreContentUpdates.sql
+++ SecurityShepherdCore/database/coreContentUpdates.sql
@@ -6,6 +6,14 @@ UPDATE modules SET week = 1 WHERE incrementalRank < 45;
 UPDATE modules SET week = 2 WHERE moduleNameLangPointer LIKE 'sql.injection%';
 UPDATE modules SET week = 3 WHERE moduleNameLangPointer LIKE 'cross.site.scripting%';
 
+UPDATE modules SET week = 4 WHERE moduleNameLangPointer = 'broken.session.management';
+UPDATE modules SET week = 4 WHERE moduleNameLangPointer = 'session.management.challenge.1';
+UPDATE modules SET week = 4 WHERE moduleNameLangPointer = 'session.management.challenge.2';
+UPDATE modules SET week = 4 WHERE moduleNameLangPointer = 'session.management.challenge.3';
+UPDATE modules SET week = 4 WHERE moduleNameLangPointer = 'cross.site.request.forgery';
+UPDATE modules SET week = 4 WHERE moduleNameLangPointer = 'csrf.1';
+UPDATE modules SET week = 4 WHERE moduleNameLangPointer = 'csrf.2';
+
 UPDATE modules SET moduleStatus = 'closed' WHERE week is null;
 UPDATE modules SET moduleStatus = 'open' WHERE week is not null;
 
