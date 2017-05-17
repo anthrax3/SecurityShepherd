95a259616284e4349d47edc898fa1091488e1ab9
Christopher McMahon <cmcmahon@walmartlabs.com>
2017-05-16 19:09:03 -0700

    Fix sql

--- SecurityShepherdCore/database/coreContentUpdates-prework.sql
+++ SecurityShepherdCore/database/coreContentUpdates-prework.sql
@@ -2,6 +2,6 @@
 
 USE `core` ;
 
-UPDATE modules SET moduleStatus = 'open' WHERE week == 0;
+UPDATE modules SET moduleStatus = 'open' WHERE week = 0;
 UPDATE modules SET moduleStatus = 'closed' WHERE week >= 1;
 COMMIT;
